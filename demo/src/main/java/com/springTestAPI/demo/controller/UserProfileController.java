18959ef2c49104cbcb8b1a66a8d03a2a5ba72986
sol lee <76844556+soleu@users.noreply.github.com>
2021-09-13 00:24:34 +0900

    put/post UserProfile

--- demo/src/main/java/com/springTestAPI/demo/controller/UserProfileController.java
+++ demo/src/main/java/com/springTestAPI/demo/controller/UserProfileController.java
@@ -2,9 +2,7 @@ package com.springTestAPI.demo.controller;
 
 import com.springTestAPI.demo.model.UserProfile;
 import org.apache.catalina.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -41,6 +39,23 @@ public class UserProfileController {
     public List<UserProfile> getAllUserProfile(){
         return new ArrayList<UserProfile>(userMap.values()); //values값만 arrayList로 변환해서 출력
     }
-    
-    
+
+    @PutMapping("/user/{id}")
+    public void putUserProfile(@PathVariable("id")String id, @RequestParam("name") String name,@RequestParam("phone")String phone,@RequestParam("address")String address){
+        UserProfile userProfile = userMap.get(id);
+        userProfile.setName(name);
+        userProfile.setPhone(phone);
+        userProfile.setAddress(address);
+    }
+
+    @PostMapping("/user/{id}")
+    public void postUserProfile(@PathVariable("id")String id, @RequestParam("name") String name,@RequestParam("phone")String phone,@RequestParam("address")String address){
+        UserProfile userProfile =new UserProfile(id,name,phone,address);
+        userMap.put(id,userProfile);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUserProfile(@PathVariable("id")String id){
+        
+    }
 }
