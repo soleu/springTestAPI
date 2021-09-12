2463a1290a2c4eac1f5775b4c529de1bba1da0e1
sol lee <76844556+soleu@users.noreply.github.com>
2021-09-13 00:02:52 +0900

    add: getAllUserProfile

--- demo/src/main/java/com/springTestAPI/demo/controller/UserProfileController.java
+++ demo/src/main/java/com/springTestAPI/demo/controller/UserProfileController.java
@@ -1,12 +1,15 @@
 package com.springTestAPI.demo.controller;
 
 import com.springTestAPI.demo.model.UserProfile;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController //controller로 인식
@@ -22,10 +25,22 @@ public class UserProfileController {
         userMap.put("4",new UserProfile("4","홍길연","111-4444","대전"));
     }
 
+    @GetMapping("/user")
+    public String getUserMain(){
+        String message = "이 페이지는 유저 메인입니다.";
+        return message;
+    }
+
     @GetMapping("/user/{id}") //path
     public UserProfile getUserProfile(@PathVariable("id")String id){
         //PathVariable :{안에들어있는 이름}을 인식하여 path로 인식
         return userMap.get(id);
     }
 
+    @GetMapping("/user/all")
+    public List<UserProfile> getAllUserProfile(){
+        return new ArrayList<UserProfile>(userMap.values()); //values값만 arrayList로 변환해서 출력
+    }
+    
+    
 }
--- demo/src/main/java/com/springTestAPI/demo/model/UserProfile.java
+++ demo/src/main/java/com/springTestAPI/demo/model/UserProfile.java
@@ -15,35 +15,5 @@ public class UserProfile {
         this.address = address;
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
